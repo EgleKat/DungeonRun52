02ea68ec388bf5daed21457d1e89fe2f1701ab2d
Serge Simakov <serge.simakov@gmail.com>
2018-04-30 21:43:41 +0100

    Animations added
    Added a quick animator to the player, so moving now looks a bit better

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Constants
 	private Rigidbody2D rb;
+	private Animator anim;
+	private SpriteRenderer sr;
 
 	private float moveSpeed = 4;
 	private Vector2 inputVector;
@@ -13,12 +15,31 @@ public class PlayerMovement : MonoBehaviour {
 
 	private void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		sr = GetComponent<SpriteRenderer>();
 	}
 
 	private void Update() {
 		//Get input
 		inputVector = new Vector2(Input.GetAxis("HorizontalMove"), Input.GetAxis("VerticalMove"));
 		diagMove = Mathf.Abs(inputVector.x * inputVector.y) != 0;
+
+		//Animator
+		int moveID = 0;
+		if (inputVector.x < 0) {
+			moveID = 1;
+			sr.flipX = false;
+		} else if (inputVector.x > 0) {
+			moveID = 2;
+			sr.flipX = true;
+		} else if (inputVector.y < 0) {
+			moveID = 3;
+			sr.flipX = false;
+		} else if (inputVector.y > 0) {
+			moveID = 4;
+			sr.flipX = false;
+		}
+		anim.SetInteger("moveID", moveID);
 	}
 
 	private void FixedUpdate() {
