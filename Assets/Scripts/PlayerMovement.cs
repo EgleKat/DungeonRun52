b3478024cf6d09c068838f72800e4a6919992b03
Serge Simakov <serge.simakov@gmail.com>
2018-04-30 20:27:37 +0100

    Created player, movement
    Added the guy who can be controlled by WASD

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Constants
+	private Rigidbody2D rb;
+
+	private float moveSpeed = 4;
+	private Vector2 inputVector;
+	private bool diagMove;
+
+	private void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
+		//Get input
+		inputVector = new Vector2(Input.GetAxis("HorizontalMove"), Input.GetAxis("VerticalMove"));
+		diagMove = Mathf.Abs(inputVector.x * inputVector.y) != 0;
+	}
+
+	private void FixedUpdate() {
+		rb.velocity = moveSpeed * inputVector * (diagMove ? 1 / Mathf.Sqrt(2) : 1);
+	}
+}
