eb670460ef7bba35dd9e7fad0dd92c5c85d9088d
Unknown <ek1g15@soton.ac.uk>
2018-05-08 19:09:02 +0100

    Weapons are random

--- Assets/Scripts/WeaponSpawn.cs
+++ Assets/Scripts/WeaponSpawn.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class WeaponSpawn : MonoBehaviour
 {
+    [HideInInspector]
     public int gunID;
 
     public Sprite[] sprites;
+    static System.Random random = new System.Random();
 
+    private void Start()
+    {
+        gunID = random.Next(1, 4);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[gunID];
+
+    }
     public void UpdateWeapon(int id)
     {
         gunID = id;
